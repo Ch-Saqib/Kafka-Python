1e5d9eea08d6dc99da8112640db78660a2d79c21
Ch Saqib <125819263+Ch-Saqib@users.noreply.github.com>
2024-05-20 13:56:09 +0500

    Kafka Ui

--- kafka_ui/kafka/main.py
+++ kafka_ui/kafka/main.py
@@ -1,6 +1,46 @@
 from fastapi import FastAPI
+from aiokafka import AIOKafkaConsumer
+from contextlib import asynccontextmanager
+import asyncio
 
-app: FastAPI = FastAPI()
+
+KAFKA_TOPIC = "Topic"
+KAFKA_SERVER = "broker:19092"
+KAFKA_GROUP_ID = "group_id"
+
+
+async def consumer():
+    consumer = AIOKafkaConsumer(
+        KAFKA_TOPIC,
+        bootstrap_servers=KAFKA_SERVER,
+    )
+    # Get cluster layout and join group `my-group`
+    await consumer.start()
+    try:
+        # Consume messages
+        async for msg in consumer:
+            print(
+                "consumed: ",
+                msg.topic,
+                msg.partition,
+                msg.offset,
+                msg.key,
+                msg.value,
+                msg.timestamp,
+            )
+    finally:
+        # Will leave consumer group; perform autocommit if enabled.
+        await consumer.stop()
+
+@asynccontextmanager
+def lifespan(app:FastAPI):
+    print("Create Connsumer")
+    asyncio.create_task(consumer())
+    print("Created")
+    yield
+
+
+app: FastAPI = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
